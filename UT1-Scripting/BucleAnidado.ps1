9a5e859602b99233ecaf92619aac4edd5a5e3a4b
AsierRodriguezO <arodriguezo02@educantabria.es>
2025-10-13 11:37:31 +0200

    final

---
+++ UT1-Scripting/BucleAnidado.ps1
@@ -0,0 +1,20 @@
+<#
+    .SYNOPSIS
+         [Creacion de bucles.]
+
+    .DESCRIPTION
+         [Primer script que creo en PowerShell para mostrar bucles.]
+
+    .PARAMETER 
+        [No hay parámetros en este script simple.]
+    .EXAMPLE
+
+    .NOTES
+        Autor: Asier
+        Fecha: 01/10/2025
+        Versión: 1.0
+        Notas: []
+#>
+
+$array = @(1,2,3)
+write-host $array
