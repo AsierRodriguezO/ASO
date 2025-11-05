08648be65c426becb36edd9a0e534fdb591e566c
AsierRodriguezO <Arodriguezo02@educantabria.es>
2025-11-05 10:46:22 +0000

    Final carpetas

--- UT1-Scripting/ContarFicheros.sh
+++ UT1-Scripting/ContarFicheros.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #==================================================================
 # Script: contarFicheros.sh
 # Descripcion: Cuenta el número de archivos en un directorio
@@ -5,12 +6,19 @@
 # Creacion: 2024-06-27
 # Comentarios: Ejemplo de como contar ficheros en un directorio dado
 #==================================================================
-
-DIRECTORIO=$1
-if [ -z "$DIRECTORIO" ]; then
-  echo "Uso: $0 <directorio>"
-  exit 1
+# Si se proporciona un directorio como argumento, usarlo; si no, usar el directorio actual
+if [ $# -eq 1 ]; then
+    Ruta="$1"
+else
+    Ruta="."
 fi
 
-NUM_FICHEROS=$(find "$DIRECTORIO" -type f | wc -l)
-echo "El directorio '$DIRECTORIO' contiene $NUM_FICHEROS archivos."
+# Verificar si el directorio existe
+if [ ! -d "$Ruta" ]; then
+    echo "Error: El directorio $Ruta no existe"
+    exit 1
+fi
+
+echo "Directorio a analizar: $Ruta"
+echo "Número de ficheros en el directorio:"
+ls -l "$Ruta" | wc -l
